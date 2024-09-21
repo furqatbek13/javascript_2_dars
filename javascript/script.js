eff65ad0e17048b3626230817fee80b1257083e2
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-22 01:11:14 +0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -30,3 +30,40 @@
 // console.log(natija);
 
 // 5-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let number4 = Math.floor(prompt("D ning qiymatini kiriting: "));
+// let natija = (((number1 / number2)-1) / (number3 / (number4 - 1)))**2;
+// console.log(natija);
+
+// 6-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let number4 = Math.floor(prompt("D ning qiymatini kiriting: "));
+// let natija = (number1 + number2) / (number3 + (number4 / (number1 + number3)));
+// console.log(natija);
+
+// 7- misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let natija = Math.sqrt(((number1 + number2 + number3)**2) - ((number1 - number2 - number3))**2);
+// console.log(natija);
+
+// 8-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let natija = ((number1**2 + number2**2 + number3**2) / (number1 * number2 * number3));
+// console.log(natija);
+
+
+// 9-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let number4 = Math.floor(prompt("D ning qiymatini kiriting: "));
+// let natija = ((number1 * (number4**3)/3) + (number2 * (number3**2)/2));
+// console.log(natija);
