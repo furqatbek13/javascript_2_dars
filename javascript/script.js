0cc6e7e131d4a2ef774a74dea6e167cd2c009af5
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-22 00:29:27 +0500

    Update script.js

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+// Uyga vazifa misollar
+
+// 1-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let natija = (number2/2) * (number1**2 * number2**2);
+// console.log(natija); 
+
+// 2-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt ("C ning qiymatini kiriting: "));
+// let number4 = Math.floor(prompt("D ning qiymatini kiriting: "))
+// let natija = ((number1 + number2) / (number3+ number4))**2 + (number2**2);
+// console.log(natija);
+
+// 3-misol 
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("C ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("D ning qiymatini kiriting: "));
+// let natija = 2 * ((number1**2) + (4* (number2**2)/3));
+// console.log(natija);
+
+// 4-misol
+// let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
+// let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
+// let number3 = Math.floor(prompt("C ning qiymatini kiriting: "));
+// let number4 = Math.floor(prompt("D ning qiymatini kiriting: "));
+// let natija = (1/ (number1**2)) * (number2 / 10)**3 * (number3 + number4)**2;
+// console.log(natija);
+
+// 5-misol
