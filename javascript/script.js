6a75a96aa8d64abd6c2f5239eb4a90a2ad519982
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-22 08:51:37 +0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@
 // let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
 // let number2 = Math.floor(prompt ("B ning qiymatini kiriting: "));
 // let natija = (number2/2) * (number1**2 * number2**2);
-// console.log(natija); 
+// console.log(natija);
 
 // 2-misol
 // let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
@@ -14,7 +14,7 @@
 // let natija = ((number1 + number2) / (number3+ number4))**2 + (number2**2);
 // console.log(natija);
 
-// 3-misol 
+// 3-misol
 // let number1 = Math.floor(prompt ("A ning qiymatini kiriting: "));
 // let number2 = Math.floor(prompt ("C ning qiymatini kiriting: "));
 // let number3 = Math.floor(prompt("D ning qiymatini kiriting: "));
@@ -67,3 +67,227 @@
 // let number4 = Math.floor(prompt("D ning qiymatini kiriting: "));
 // let natija = ((number1 * (number4**3)/3) + (number2 * (number3**2)/2));
 // console.log(natija);
+
+
+// 10-misol
+
+
+
+
+// Boolean- 1
+// let number = Math.floor(prompt("Bironta son kiriting: "));
+// if ( number % 2 == 0){
+//     console.log("kiritgan soningiz juft.");
+// }else {
+//     console.log("kiritgan soning toq");
+// }
+
+
+// Boolean- 2
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// if (number1 >= 0 ||  number2 < -2) {
+//     console.log("A >= 0 yoki B < -2.");
+// }else{
+//     console.log("Shart bajarilmaydi: A >= 0 yoki B < -2 emas.");
+// }
+
+
+// Boolean- 3
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// let number3 = Math.floor(prompt("C sonni kiriting: "));
+// if ((number2 > number1 && number2 < number3) || (number2 > number3 && number2 < number1)) {
+//     console.log("B soni A va C sonlari orasida yotadi.");
+// }else{
+//     console.log("B soni A va C sonlari orasida yotmaydi.");
+// }
+
+
+// Boolean- 4
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// if (number1 % 2 == 0 && number2 % 2 == 0) {
+//     console.log("A va B sonlari toq sonlar.");
+// }else {
+//     console.log("A va B sonlari toq sonlar emas.");
+// }
+
+
+// Boolean- 5
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// if (number1 % 2 !== 0 || number2 % 2 !== 0){
+//     console.log("A va B sonlarning hech bo'lmaganda bittasi toq son.");
+// }else {
+//     console.log("A va B sonlari toq emas");
+// }
+
+
+
+// Boolean- 6
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// if ((number1 % 2 !== 0 && number2 % 2 === 0) || (number1 % 2 === 0 && number2 % 2 !== 0)) {
+//     console.log("A va B sonlarning faqat bittasi toq son.");
+// } else {
+//     console.log("A va B sonlarning faqat bittasi toq emas.");
+// }
+
+
+
+// Boolean- 7
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// let number3 = Math.floor(prompt ("C sonni kiriting"));
+// if (number1 > 0 && number2 > 0 && number3 > 0) {
+//     console.log("A, B, C sonlarning har biri musbat.");
+// } else {
+//     console.log("A, B, C sonlarning har biri musbat emas.");
+// }
+
+
+
+
+// Boolean- 8
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// let number3 = Math.floor(prompt ("C sonni kiriting"));
+// if ((number1 > 0 && number2 <= 0 && number3 <= 0) ||
+//     (number1 <= 0 && number2 > 0 && number3 <= 0) ||
+//     (number1 <= 0 && number2 <= 0 && number3 > 0)) {
+//     console.log("A, B, C sonlaridan faqat bittasi musbat son.");
+// } else {
+//     console.log("A, B, C sonlaridan faqat bittasi musbat emas.");
+// }
+
+
+
+// Boolean- 9
+
+
+
+// Boolean- 10
+// let number = Math.floor(prompt("ikki xonali son kiriting: "));
+// if (number >= 10 && number < 100 && number % 2 === 0) {
+//     console.log("Berilgan son ikki xonali juft son.");
+// } else {
+//     console.log("Berilgan son ikki xonali juft son emas.");
+// }
+
+
+// Boolean- 11
+// let number = Math.floor(prompt("uch xonali son kiriting: "));
+// if (number >= 100 && number < 1000 && number % 2 === 0){
+//     console.log("Berilgan son uch xonali juft son.");
+// }else{
+//     console.log("Berilgan son uch xonali juft son emas.");
+// }
+
+
+// Boolean- 12
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// let number3 = Math.floor(prompt ("C sonni kiriting"));
+// if ((number1 > 0 && number2 < 0) || (number1 > 0 && number3 < 0) || (number2 > 0 && number3 < 0) ||
+//     (number1 < 0 && number2 > 0) || (number1 < 0 && number3 > 0) || (number2 < 0 && number3 > 0)) {
+//     console.log("Berilgan uchta butun sonlarning hech bo'lmaganda bir jufti o'zaro qarama-qarshi.");
+// } else {
+//     console.log("Berilgan uchta butun sonlarning hech bo'lmaganda bir jufti o'zaro qarama-qarshi emas.");
+// }
+
+
+
+// Boolean- 13
+// let number = Math.floor(prompt("uch xonali son kiriting: "));
+// let yuzlarXonasi = Math.floor(number / 100);
+// let onlarXonasi = Math.floor((number % 100) / 10);
+// let birlarXonasi =  number % 10;
+// if (yuzlarXonasi < onlarXonasi && onlarXonasi < birlarXonasi) {
+//     console.log("Ushbu sonning raqamlari ketma-ket o'suvchi bo'lib joylashgan.");
+// } else {
+//     console.log("Ushbu sonning raqamlari ketma-ket o'suvchi emas.");
+// }
+
+
+// Boolean- 14
+// let number = Math.floor(prompt("uch xonali son kiriting: "));
+// let yuzlarXonasi = Math.floor(number / 100);
+// let onlarXonasi = Math.floor((number % 100) / 10);
+// let birlarXonasi =  number % 10;
+// if ((yuzlarXonasi < onlarXonasi && onlarXonasi < birlarXonasi) || (yuzlarXonasi > onlarXonasi && onlarXonasi > birlarXonasi)) {
+//     console.log("Ushbu sonning raqamlari ketma-ket o'suvchi bo'lib joylashgan yoki kamayuvchi ketma-ketlikka ega.");
+// } else {
+//     console.log("Ushbu sonning raqamlari o'suvchi yoki kamayuvchi emas.");
+// }
+
+
+
+// Boolean- 15
+// let number = Math.floor(prompt("uch xonali son kiriting: "));
+// let yuzlarXonasi = Math.floor(number / 100);
+// let onlarXonasi = Math.floor((number % 100) / 10);
+// let birlarXonasi =  number % 10;
+// if (yuzlarXonasi === onlarXonasi) {
+//     console.log("Ushbu sonni chapdan o'qiganda ham, o'ngdan o'qiganda ham bir xil.");
+// } else {
+//     console.log("Ushbu sonni chapdan o'qiganda ham, o'ngdan o'qiganda ham bir xil emas.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Boolean- 1
+// let numer = Math.floor(prompt("Bironta son kiriting: "));
+// if (numer > 0) {
+//     console.log("kiritgan soningiz musbat.");
+// } else {
+//     console.log("kiritgan soningiz musbat emas.");
+// }
+
+
+// Boolean- 2
+// let number = Math.floor(prompt("Bironta son kiriting: "));
+// if ( number % 2 == 0){
+//     console.log("kiritgan soningiz juft.");
+// }else {
+//     console.log("kiritgan soning toq");
+// }
+
+// Boolean- 3
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// if (number1 > 2 &&  number2 <= 3) {
+//     console.log("kiritgan soningiz 2 dan katta 3 dan kichik son");
+// }else{
+//     console.log("kiritgan soning 2 dan katta ham 3 dan ham katta");
+// }
+
+
+// Boolean- 4
+// let number1 = Math.floor(prompt("A sonni kiriting: "));
+// let number2 = Math.floor(prompt("B sonni kiriting: "));
+// let number3 = Math.floor(prompt("C sonni kiriting: "));
+// if (number1 <= number2 && number2 <= number3) {
+//     console.log("A <= B <= C. shartni qanotlantiradi kiritgan soningiz");
+// }else{
+//     console.log("Shart bajarilmaydi: A <= B <= C emas.");
+// }
+
+
+
+
